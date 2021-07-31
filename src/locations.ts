6f4b8657e09e29365baa04e8ce9aa62617bc9473
MapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2021-07-31 14:09:37 -0500

    some location stuff

---
+++ src/locations.ts
@@ -0,0 +1,48 @@
+import { NonEmptyArray, ValueOf } from "./types.ts"
+
+export type LocationId = ValueOf<typeof locationIds>
+
+export type Location = {
+  name: string
+  description: string
+  exits: NonEmptyArray<LocationId>
+}
+
+export function getInitialLocationId(): LocationId {
+  return locationIds[0]
+}
+
+export function getInitialLocation(): Location {
+  return locationMap[locationIds[0]]
+}
+
+export function getLocation(locationId: LocationId): Location {
+  return locationMap[locationId]
+}
+
+export function getAllLocations(): Location[] {
+  return Object.values(locationMap)
+}
+
+const locationIds = ["tavern", "townSquare", "forest"] as const
+
+const locationMap: Record<LocationId, Location> = {
+  tavern: {
+    name: "The Tavern",
+    description:
+      "The tavern is a large building with a bar and some tables. There are several people here, including a local barwoman and several customers.",
+    exits: ["townSquare"],
+  },
+  townSquare: {
+    name: "The Town Square",
+    description:
+      "The town square is a large open space with a few buildings and shop stalls, paths lined with cobblestone, and a fountain in the middle of the square.",
+    exits: ["tavern", "forest"],
+  },
+  forest: {
+    name: "The Forest",
+    description:
+      "The forest is a large area sporting a bounty of colorful foliage and wildlife. If you go deeper, you might find something interesting. But I haven't coded that in yet.",
+    exits: ["townSquare"],
+  },
+}
--- src/types.ts
+++ src/types.ts
@@ -1,1 +1,5 @@
 export type MaybePromise<T> = Promise<T> | T
+
+export type NonEmptyArray<T> = [T, ...T[]]
+
+export type ValueOf<T> = T extends readonly unknown[] ? T[number] : T
